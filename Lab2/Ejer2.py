7010a59c20b85e1bb16c65f81a5a6b3fb6c290be
Diego77Blaze <48413305+Diego77Blaze@users.noreply.github.com>
2020-02-19 18:20:44 +0100

    Content updated

--- Lab2/Ejer2.py
+++ Lab2/Ejer2.py
@@ -9,13 +9,32 @@ class Archivo:
     def __init__(self, l, a):
         self.longitud = l
         self.accesos = a
+        self.longitudporaccesos = l/a
 
 
 listaArchivos = []
-
-def ordenarArchivos(listaArchivos):
-    for i in range(6):
+def crearArray(listaArchivos):
+    for i in range(len(ficheros)-1):
         listaArchivos.append(Archivo(ficheros[i], peticiones[i]))
+        print(listaArchivos[i].longitud, listaArchivos[i].accesos, listaArchivos[i].longitudporaccesos)
+    print()
+    return listaArchivos
+
+
+def bubbleSort(listaArchivos):
+    for lon in range(len(listaArchivos)-1, 0, -1):
+        for i in range(lon):
+            if listaArchivos[i].longitudporaccesos>listaArchivos[i+1].longitudporaccesos:
+                temp = listaArchivos[i]
+                listaArchivos[i] = listaArchivos[i+1]
+                listaArchivos[i+1] = temp
+
+
+bubbleSort(crearArray(listaArchivos))
+for i in range (len(listaArchivos)):
+    print(listaArchivos[i].longitud, listaArchivos[i].accesos, listaArchivos[i].longitudporaccesos)
+
+
 
 
 

8f339058ac1fcd1cb91b80f70ff4fbc72a9ac345
Diego77Blaze <48413305+Diego77Blaze@users.noreply.github.com>
2020-02-29 12:42:11 +0100

    Content Updated

--- Lab2/Ejer5.py
+++ Lab2/Ejer5.py
@@ -1,27 +1,42 @@
-import sys
-def paraVisitar():
-    v = -1
-    for i in range(numNodos):
-        if visitadoydistancia[i][0] == 0 and (v<0 or visitadoydistancia)
+grafo = {'S':{'B': 5, 'C':2},'B':{'D':4, 'E': 2},'C':{'B':8,'E':7}, 'D':{'E':6,'F':3},'E':{'F':1}} #Creamos el grafo que usaremos de ejemplo
 
 
-costes = [[0, 5, 2, 0, 0, 0],
-          [0, 0, 0, 4, 2, 0],
-          [0, 8, 0, 0, 7, 0],
-          [0, 0, 0, 0, 6, 3],
-          [0, 0, 0, 0, 0, 1],
-          [0, 0, 0, 0, 0, 0]]
+def dijkstra(grafo, nodoInicial, nodoMeta):
+    distanciaMasCorta = {}
+    predecesor = {}
+    nodosNoVistos = grafo
+    nodosRuta = []
+    for nodo in nodosNoVistos:
+        distanciaMasCorta[nodo] = 1000
+    distanciaMasCorta[nodoInicial] = 0
 
-nodos = [[0, 1, 1, 0, 0, 0],
-         [0, 0, 0, 1, 1, 0],
-         [0, 1, 0, 0, 1, 0],
-         [0, 0, 0, 0, 1, 1],
-         [0, 0, 0, 0, 0, 1],
-         [0, 0, 0, 0, 0, 0]]
-global visitadoydistancia
-numNodos = len(nodos[0])
+    while nodosNoVistos:
+        nodoMinimo = None
+        for nodo in nodosNoVistos:
+            if nodoMinimo is None:
+                nodoMinimo = nodo
+            elif distanciaMasCorta[nodo]<distanciaMasCorta[nodoMinimo]:
+                nodoMinimo = nodo
 
+        for conectado, peso in grafo[nodoMinimo].items():
+            if peso + distanciaMasCorta[nodoMinimo] < distanciaMasCorta[conectado]:
+                distanciaMasCorta[conectado] = peso + distanciaMasCorta[nodoMinimo]
+                predecesor[conectado] = nodoMinimo
+            nodosNoVistos.pop(nodoMinimo)
 
+            nodoActual = nodoMeta
+            while nodoActual != nodoInicial:
+                try:
+                    nodosRuta.insert(0, nodoActual)
+                    nodoActual = predecesor[nodoActual]
+                except KeyError:
+                    print('Path not reachable')
+                    break
+            nodosRuta.insert(0, nodoInicial)
+            if distanciaMasCorta[nodoMeta] != 1000:
+                print('Shortest distance is ' + str(distanciaMasCorta[nodoMeta]))
+                print('And the path is ' + str(nodosRuta))
 
+        dijkstra(grafo, 'a', 'b')
 
 

eebaf32b9eacb3a6ea42365f15f05b9817715e87
Diego77Blaze <48413305+Diego77Blaze@users.noreply.github.com>
2020-02-29 11:31:50 +0100

    Content Updated

---
+++ Lab2/Ejer5.py
@@ -0,0 +1,27 @@
+import sys
+def paraVisitar():
+    v = -1
+    for i in range(numNodos):
+        if visitadoydistancia[i][0] == 0 and (v<0 or visitadoydistancia)
+
+
+costes = [[0, 5, 2, 0, 0, 0],
+          [0, 0, 0, 4, 2, 0],
+          [0, 8, 0, 0, 7, 0],
+          [0, 0, 0, 0, 6, 3],
+          [0, 0, 0, 0, 0, 1],
+          [0, 0, 0, 0, 0, 0]]
+
+nodos = [[0, 1, 1, 0, 0, 0],
+         [0, 0, 0, 1, 1, 0],
+         [0, 1, 0, 0, 1, 0],
+         [0, 0, 0, 0, 1, 1],
+         [0, 0, 0, 0, 0, 1],
+         [0, 0, 0, 0, 0, 0]]
+global visitadoydistancia
+numNodos = len(nodos[0])
+
+
+
+
+

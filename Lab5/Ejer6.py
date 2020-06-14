35c007451a0a7873bdc04ad539ecd0909f0c0a10
Diego77Blaze <48413305+Diego77Blaze@users.noreply.github.com>
2020-06-14 13:59:40 +0200

    Create Ejer6.py

---
+++ Lab5/Ejer6.py
@@ -0,0 +1,40 @@
+def cambiarCaracter(car1, car2, listaCadena, pos):
+    char = car1+car2
+    tabla = {
+        "aa": "b",
+        "ab": "b",
+        "ac": "a",
+        "ad": "d",
+        "ba": "c",
+        "bb": "a",
+        "bc": "d",
+        "bd": "a",
+        "ca": "b",
+        "cb": "a",
+        "cc": "c",
+        "cd": "c",
+        "da": "d",
+        "db": "c",
+        "dc": "d",
+        "dd": "b"
+    }
+    posibLista = listaCadena.copy()
+    posibLista[pos] = tabla.get(char)
+    posibLista.pop(pos+1)
+    return posibLista
+
+def backtrackingCadena(cad):
+    if(len(cad)==1):
+        if cad[0] not in validos:
+            validos.append(cad[0])
+    else:
+        for i in range(len(cad)-1):
+            posibleCad = cambiarCaracter(cad[i], cad[i+1],cad, i)
+            backtrackingCadena(posibleCad)
+
+
+cadena = "acabada"
+lista = list(cadena)
+validos = []
+backtrackingCadena(lista)
+print(validos)

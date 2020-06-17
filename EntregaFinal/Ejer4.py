57e6ecd6aebeeb86faca50e75839e80f21f71fe8
Diego77Blaze <48413305+Diego77Blaze@users.noreply.github.com>
2020-06-17 17:29:04 +0200

    Update Ejer4.py

--- EntregaFinal/Ejer4.py
+++ EntregaFinal/Ejer4.py
@@ -4,13 +4,16 @@ lado1 = int(lista[0][0])
 lado2 = int(lista[1][0])
 print(lado1, lado2)
 matriz = []
-for i in range(2, lado2+1):
+for i in range(2, lado2+2):
     linea = lista[i].split()
     lineaMatriz = []
     for elem in linea:
         lineaMatriz.append(elem)
     matriz.append(lineaMatriz)
-print(matriz)
+
+
+
+
 
 
 
